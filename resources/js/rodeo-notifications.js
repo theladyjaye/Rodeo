2543615cb6e884dfc34c562d9e99ef65e2e15f06
Adam Venturella <aventurella@gmail.com>
2011-05-02 20:27:59 -0700

    base CSS stuff and modal logic

--- resources/js/rodeo-notifications.js
+++ resources/js/rodeo-notifications.js
@@ -2,7 +2,15 @@ var rodeo = rodeo || { }
 rodeo.notifications = { }
 rodeo.notifications.alert = function(object)
 {
-	alert(object.message);
+	var template = $("#modal-alert").html();
+	var model = {"title"   : (typeof object.title       == "undefined" ? "Alert" : object.title),
+	             "message" : (typeof object.message     == "undefined" ? "Something happened" : object.message),
+	             "label"   : (typeof object.buttonLabel == "undefined" ? "Ok" : object.buttonLabel)}
+	var view = $(Mustache.to_html(template, model));
+	
+	view.modal({closeClass:".actions .button",
+		        opacity:80,
+		        overlayCss: {backgroundColor:"#000"}});
 }
 
 rodeo.notifications.confirmation = function(object)
